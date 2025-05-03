bfeaf77c8c610093a5855d415238c28ea1c3afb7
H-SPACES <cardonaosoriosamana@gmail.com>
2025-05-03 13:47:31 -0500

    Primer commit del proyecto

---
+++ Assets/fuego_vertical.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fuego_vertical : MonoBehaviour
+
+   {
+    public float amplitud = 2f;        // Qué tan grande es el radio de movimiento
+    public float velocidad = 2f;       // Velocidad de rotación
+    public float fase = 0f;            // Desfase para posicionar en la cuerda
+    public float intensidadCentro = 1f; // 1 en el centro, 0 en los extremos
+    private Vector3 centro;
+
+    void Start()
+    {
+        centro = transform.parent.position;
+    }
+    void LateUpdate()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+        float angulo = Time.time * velocidad + fase;
+        float y = Mathf.Sin(angulo) * amplitud * intensidadCentro;
+        float z = Mathf.Cos(angulo) * amplitud;
+        float x = transform.localPosition.x;
+        transform.localPosition = new Vector3(x, y, z);
+
+    }
+}
